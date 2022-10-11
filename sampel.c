87919d3adc94c1db72e26929a53ac2a182baf447
asbjørn <qw41ha@student.aau.dk>
2022-10-11 20:06:45 +0200

    laver en funsjon

--- sampel.c
+++ sampel.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <limits.h>
 
+void print_biner(int i_spen)
+{
+	int i_m = INT_MIN;
+	unsigned int i = *(int*)(&i_m);
+	while((i_spen & i) == 0)
+		i = i >> 1;
+	while(i != 0)
+	{
+		printf((i_spen & i) == 0 ? "0" : "1");
+		i = i >> 1;
+	}
+	printf("\n");
+}
+
 int main()
 {	
 	double vcc;
 	double spe;
-	int i_m = INT_MIN;
-	unsigned int i = *(int*)(&i_m);
 	int bit;
 	printf("skriv cvv:");
 	scanf("%lf",&vcc);
@@ -19,14 +31,8 @@ int main()
 
 	int i_spen = (int)((spe/vcc)*bit);
 	printf("speningen er %d eller ",i_spen);
+	print_biner(i_spen);
 
-	while((i_spen & i) == 0)
-		i = i >> 1;
-	while(i != 0)
-	{
-		printf((i_spen & i) == 0 ? "0" : "1");
-		i = i >> 1;
-	}
-	printf("\n");
+
 	return 0;
 }
